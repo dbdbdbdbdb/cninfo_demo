1692a14be30aae569b07d317d80f757c6f87f762
XinboZhang <zhangxinbo@pku.edu.cn>
2018-01-23 23:47:40 +0800

    add：图层展开

--- main/query.py
+++ main/query.py
@@ -10,6 +10,7 @@ from gstore_api.GstoreConnector import GstoreConnector
 import template_search as ts
 import json
 
+
 def entity_search(str):
     # 长于四个字,认为是机构
     if len(str) > 4:
@@ -21,7 +22,9 @@ def entity_search(str):
     # with open('search1_result.txt', 'r', encoding='utf-8') as f:
     #     answer = f.read()
     # os.remove(os.path.dirname(os.path.abspath(__file__))+'\\search1_result.txt')
-    json_str = conv_node_edge.conv2graph(answer)
+    # print(answer)
+    json_str = conv_node_edge.conv2pgraph(answer)
+    # print(json_str)
     # with open('search1_result.txt', 'w', encoding='utf-8') as f:
     #     f.write(json_str)
     return json_str
@@ -42,7 +45,7 @@ def query_result(sparql):
     # }'''
     # 因为最后一行是NUT字符,所以切片掉
     answer = (gc.query(sparql))
-    #print(answer)
+    # print(answer)
 
     # print(answer)
     with open('search1_result.txt', 'w', encoding='utf-8') as f:
@@ -56,58 +59,72 @@ def plus_search(data_id):
     json_plus = conv_node_edge.plus2graph(answer, data_id)
     return json_plus
 
+
+def extend_search(data_id):
+    sparql = 'select ?y ?z\n{\n\t<' + data_id + '> ?y  ?z.\n}'
+    answer = query_result(sparql)
+    json_plus = conv_node_edge.extend2graph(data_id)
+    return json_plus
+
+
 # answer = entity_search("南华生物医药股份有限公司")
 # with open('results_end.txt', 'w', encoding='utf-8') as f:
 #     f.write(answer)
 def template_search(str_to_solve, select_object):
-    sparql=''
-    if select_object=='securityID':
+    sparql = ''
+    if select_object == 'securityID':
         sparql = ts.sparqlquery4(str_to_solve)
-    elif select_object=='penalty':
+    elif select_object == 'penalty':
         sparql = ts.sparqlquery2(str_to_solve)
-    elif select_object=='lawsuit':
+    elif select_object == 'lawsuit':
         sparql = ts.sparqlquery3(str_to_solve)
-    elif select_object=='manager':
+    elif select_object == 'manager':
         sparql = ts.sparqlquery5(str_to_solve)
-    elif select_object=='capitalchange':
+    elif select_object == 'capitalchange':
         sparql = ts.sparqlquery1(str_to_solve)
-    answer=query_result(sparql)
-    #with open('sparql_answer.txt', 'w', encoding='utf-8') as f:
+    answer = query_result(sparql)
+    # with open('sparql_answer.txt', 'w', encoding='utf-8') as f:
     #    f.write(answer)
-    answer_list=conv_node_edge.to_triples_list(answer)
-    #conv_table.group2table(answer_list)
-    json_list=conv_table.conv2table(answer_list)
-    json_list=json.dumps(json_list)
+    answer_list = conv_node_edge.to_triples_list(answer)
+    # conv_table.group2table(answer_list)
+    json_list = conv_table.conv2table(answer_list)
+    json_list = json.dumps(json_list)
     return json_list
+
 
 # print(template_search('公司的证券号是多少？'))
 # template_search('南华生物医药股份有限公司的证券号是多少')
+def getJsonNext():
+    tmp = conv_node_edge.getJsonNext()
+    return tmp
+
 
 def relation_search(entity1, entity2):
-    #entity1和entity2都是公司
-    if len(entity1)>4 and len(entity2)>4:
+    # entity1和entity2都是公司
+    if len(entity1) > 4 and len(entity2) > 4:
         pred1 = '<http://cn.info/vocab/organization_ORGNAME>'
         pred2 = '<http://cn.info/vocab/organization_ORGNAME>'
-    #entity1是公司，entity2是个人
-    elif len(entity1)>4 and len(entity2)<=4:
+    # entity1是公司，entity2是个人
+    elif len(entity1) > 4 and len(entity2) <= 4:
         pred1 = '<http://cn.info/vocab/organization_ORGNAME>'
         pred2 = '<http://cn.info/vocab/naturalperson_PERSONNAME>'
-    #entity1是个人，entity2是公司
-    elif len(entity1)<=4 and len(entity2)>4:
+    # entity1是个人，entity2是公司
+    elif len(entity1) <= 4 and len(entity2) > 4:
         pred1 = '<http://cn.info/vocab/naturalperson_PERSONNAME>'
         pred2 = '<http://cn.info/vocab/organization_ORGNAME>'
-    #entity1和entity2都是个人
+    # entity1和entity2都是个人
     else:
         pred1 = '<http://cn.info/vocab/naturalperson_PERSONNAME>'
         pred2 = '<http://cn.info/vocab/naturalperson_PERSONNAME>'
-    sparql='select ?x1 ?y1 ?z ?y2 ?x2\n' + \
-               '{\n' + \
-               '\t?x1 ' + pred1 + ' "' + entity1 + '".\n' + \
-               '\t?x2 ' + pred2 + ' "' + entity2 + '".\n' + \
-               '\t?x1 ?y1 ?z.\n' + \
-               '\t?z ?y2 ?x2.\n}'
+    sparql = 'select ?x1 ?y1 ?z ?y2 ?x2\n' + \
+             '{\n' + \
+             '\t?x1 ' + pred1 + ' "' + entity1 + '".\n' + \
+             '\t?x2 ' + pred2 + ' "' + entity2 + '".\n' + \
+             '\t?x1 ?y1 ?z.\n' + \
+             '\t?z ?y2 ?x2.\n}'
     answer = query_result(sparql)
-    answer_list = conv_node_edge.to_relation_list(answer)
-    json_list = json.dumps(answer_list)
-
+    (nodes, links) = conv_node_edge.to_relation_list(answer)
+    data_dict = conv_node_edge.conv2graph_dict(nodes, links)
+    json_list = json.dumps(data_dict)
+    print(json_list)
     return json_list
